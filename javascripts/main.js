33cc1bef9a61a8deec72c10dd93cd5c3809c9305
Nathan Greenstein <nathang345@gmail.com>
2014-02-16 11:24:41 -0800

    Appearance improvements to page.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,14 @@
-var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
-
-  if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
-  } else {
-    $section.css('height','auto');
-  }
-}
+// var sectionHeight = function() {
+//   var total    = $(window).height(),
+//       $section = $('section').css('height','auto');
+// 
+//   if ($section.outerHeight(true) < total) {
+//     var margin = $section.outerHeight(true) - $section.height();
+//     $section.height(total - margin - 20);
+//   } else {
+//     $section.css('height','auto');
+//   }
+// }
 
 $(window).resize(sectionHeight);
 
@@ -27,7 +27,7 @@ $(document).ready(function(){
     event.preventDefault();    
   });
   
-  sectionHeight();
+  // sectionHeight();
   
   $('img').load(sectionHeight);
 });
